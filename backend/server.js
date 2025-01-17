e7284f96ca7ba1faede46ed8ba545586fd6c163f
CristianUsiña <158125455+CrisLearn@users.noreply.github.com>
2025-01-17 00:00:48 -0500

    cambio

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from './src/app.js'; // Importar la aplicación desde app.js
-
-const PORT = process.env.PORT || 5000;
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
